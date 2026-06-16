cf47205d063354af0bcd530420fac0e558575069
r-tanaka <rtanaka0073@gmail.com>
2026-06-16 14:39:04 +0900

    レスポンシブ対応

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// ドリンクとフードの切り替え
 const drinkBtn = document.getElementById("drink-button");
 const foodBtn = document.getElementById("food-button");
 
@@ -19,3 +20,13 @@ foodBtn.addEventListener("click", () => {
   foodBtn.classList.add("active");
   drinkBtn.classList.remove("active");
 });
+
+// ヒーロースライドショー
+const slides = document.querySelectorAll('.slide');
+let index = 0;
+
+setInterval(() => {
+  slides[index].classList.remove('active');
+  index = (index + 1) % slides.length;
+  slides[index].classList.add('active');
+}, 4000);
